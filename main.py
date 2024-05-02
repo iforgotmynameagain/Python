eacffe228ddc18047507c23713f78746422763bf
iforgotmynameagain <158217314+iforgotmynameagain@users.noreply.github.com>
2024-05-02 17:37:24 +0000

    Lesson_5 variables

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
-print('hell"o world')
-
-#print(but nothing)
+cup = input('в кружке: ')
+glass = input('в стакане: ')
+print('в кружке', cup, ', а в стакане', glass)
+bag = cup
+cup = glass
+glass = bag
+print(
+    'в пакет мы перелили', cup, ', в кружку перелили', glass +
+    ', в стакан слили пакет и теперь в кружке ' + cup + ', а в стакане', glass)
+#check git
